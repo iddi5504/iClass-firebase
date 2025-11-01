ea7236dfdc0dcba08ba489d80ab9353369dc4e96
iddi <iddi5504@gmail.com>
2025-11-01 05:33:32 +0000

    improve and document code

--- src/views/takeTest.vue
+++ src/views/takeTest.vue
@@ -1,61 +1,64 @@
 <template>
-   <div class="h-100  d-flex flex-column justify-content-center ">
-    <div class="body mh-10 mw-80 d-flex flex-column justify-content-around align-items-center w-100">
-        <div class="header h1 text-center">
-           Take an iClass test
-        </div>
-        <div class="inputfields d-flex flex-column align-items-start justify-content-around">
-            <label for="username">Student's name</label>
-            <div style="position:relative;">
-                <input @keyup.enter="takeTest" v-model.lazy="studentName" type="text" id="username">
-                <i class="bi bi-blockquote-right"></i>
+    <div class="h-100  d-flex flex-column justify-content-center ">
+        <div class="body mh-10 mw-80 d-flex flex-column justify-content-around align-items-center w-100">
+            <div class="header h1 text-center">
+                Take an iClass test
             </div>
+            <div class="inputfields d-flex flex-column align-items-start justify-content-around">
+                <label for="username">Student's name</label>
+                <div style="position:relative;">
+                    <input @keyup.enter="takeTest" v-model.lazy="studentName" type="text" id="username">
+                    <i class="bi bi-blockquote-right"></i>
+                </div>
+            </div>
+            <div class="text-center note  d-flex flex-column align-items-start justify-content-around">
+                Your student name is the name your teacher knows you by.Make sure they are familiar with it
+            </div>
+
+            <button @click="takeTest()">Take a test</button>
+
+
         </div>
-        <div class="text-center note  d-flex flex-column align-items-start justify-content-around">
-            Your student name is the name your teacher knows you by.Make sure they are familiar with it 
-        </div>
-
-        <button @click="takeTest()">Take a test</button>
-
-
     </div>
-   </div>
 </template>
 
 <script>
 export default {
-    data(){
+    data() {
         return {
-            studentName:""
+            studentName: ""
         }
     },
-    methods:{
-        takeTest(){
-            this.$store.dispatch('studentName',this.studentName)
-            .then(()=>{
-                this.studentName ? this.$router.push({name:'searchQuestion'}) : console.log('fill out your name')
-            })
-            
+    methods: {
+        takeTest() {
+            this.$store.dispatch('studentName', this.studentName)
+                .then(() => {
+                    this.studentName ? this.$router.push({ name: 'searchQuestion' }) : alert("Please enter your name")
+                })
+
         }
     }
 }
 </script>
 
 <style scoped>
-.body{
+.body {
     max-height: 55vh;
     height: 100%;
 }
+
 .note {
     max-width: 34ch;
 }
+
 .bi-blockquote-right {
     position: absolute;
     top: 4px;
     font-size: 1.6rem;
     left: 12px;
 }
-input{
+
+input {
     padding-left: 39px;
 }
 </style>
